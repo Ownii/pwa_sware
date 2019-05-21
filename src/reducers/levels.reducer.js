4a93363f74e3ff3fa309008c8fdce900d4033347
Martin Förster <martin.foerster@brickmakers.de>
2019-05-21 10:26:07 +0200

    new demo level

--- src/reducers/levels.reducer.js
+++ src/reducers/levels.reducer.js
@@ -7,15 +7,17 @@ import {
     BLOCK_TYPE_WALL,
     COLOR_GREY,
     COLOR_LIME,
-    COLOR_BLUE
+    COLOR_BLUE,
+    COLOR_PURPLE,
+    COLOR_ORANGE
 } from '../utils/constants';
 
 const initialState = Map({
     levels: List.of(
         Map({
             id: 1,
-            possibleIn: 3,
-            size: 5,
+            possibleIn: 9,
+            size: 7,
             blocks: List.of(
                 {
                     type: BLOCK_TYPE_MOVE,
@@ -26,32 +28,80 @@ const initialState = Map({
                 {
                     type: BLOCK_TYPE_MOVE,
                     color: COLOR_LIME,
-                    x: 1,
+                    x: 6,
                     y: 0
                 },
                 {
                     type: BLOCK_TYPE_MOVE,
                     color: COLOR_BLUE,
                     x: 0,
-                    y: 1
+                    y: 6
+                },
+                {
+                    type: BLOCK_TYPE_MOVE,
+                    color: COLOR_ORANGE,
+                    x: 6,
+                    y: 6
                 },
                 {
                     type: BLOCK_TYPE_TARGET,
                     color: COLOR_RED,
-                    x: 4,
-                    y: 4
+                    x: 2,
+                    y: 2
+                },
+                {
+                    type: BLOCK_TYPE_WALL,
+                    color: COLOR_GREY,
+                    x: 2,
+                    y: 1
+                },
+                {
+                    type: BLOCK_TYPE_WALL,
+                    color: COLOR_GREY,
+                    x: 1,
+                    y: 2
                 },
                 {
                     type: BLOCK_TYPE_WALL,
                     color: COLOR_GREY,
                     x: 4,
+                    y: 1
+                },
+                {
+                    type: BLOCK_TYPE_WALL,
+                    color: COLOR_GREY,
+                    x: 5,
+                    y: 2
+                },
+                {
+                    type: BLOCK_TYPE_WALL,
+                    color: COLOR_GREY,
+                    x: 1,
+                    y: 4
+                },
+                {
+                    type: BLOCK_TYPE_WALL,
+                    color: COLOR_GREY,
+                    x: 2,
+                    y: 5
+                },
+                {
+                    type: BLOCK_TYPE_WALL,
+                    color: COLOR_GREY,
+                    x: 4,
+                    y: 5
+                },
+                {
+                    type: BLOCK_TYPE_WALL,
+                    color: COLOR_GREY,
+                    x: 5,
+                    y: 4
+                },
+                {
+                    type: BLOCK_TYPE_WALL,
+                    color: COLOR_GREY,
+                    x: 3,
                     y: 3
-                },
-                {
-                    type: BLOCK_TYPE_WALL,
-                    color: COLOR_GREY,
-                    x: 0,
-                    y: 4
                 }
             )
         })
